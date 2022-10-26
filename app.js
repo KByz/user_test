ecf7090e610c4c2d1176cf162bbaab00f9e32842
Kira Byrne <kira-byrne@live.nmit.ac.nz>
2022-10-27 11:31:24 +1300

    firebase install

---
+++ app.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBmrAfouESZoIOFzsmvLFbPb_wns2ItjlY",
+  
+    authDomain: "tmrrw-f9593.firebaseapp.com",
+  
+    databaseURL: "https://tmrrw-f9593-default-rtdb.firebaseio.com",
+  
+    projectId: "tmrrw-f9593",
+  
+    storageBucket: "tmrrw-f9593.appspot.com",
+  
+    messagingSenderId: "191904047568",
+  
+    appId: "1:191904047568:web:019f9765e51bbd7c4159d5"
+  
+  };
+
+  
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
